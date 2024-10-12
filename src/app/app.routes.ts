28569098e8191b91d646217434a4f6bfc64ce1ed
Pablo Carpi <upwebcomar@gmail.com>
2024-10-12 20:31:43 -0300

    prueba de modal con ng-bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 export const routes: Routes = [
-    {path:"",component:HomeComponent}
+    {path:"",component:HomeComponent},
+    {path:"modal",component:ModalComponent}
 ];
