12733ff2166a5e43b5a423401805601354735280
Pablo Carpi <upwebcomar@gmail.com>
2025-02-04 18:38:13 -0300

    Ajuste en pdf-preview. ahora se llama file-preview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ import { RoleGuard } from './auth/guards/role.guard';
 import { UnauthorizedComponent } from './views/unauthorized/unauthorized.component';
 import { NotFoundComponent } from './views/not-found/not-found.component';
 import { AuthGuard } from './auth/guards/auth.guard';
-import { PdfPreviewComponent } from './views/pdf-preview/pdf-preview.component';
+import { FilePreviewComponent } from './views/pdf-preview/file-preview.component';
 import { CrearAnuncioComponent } from './views/tableroanuncios/forms/crear-anuncio.component';
 import { EditarAnuncioComponent } from './views/tableroanuncios/forms/editar-anuncio.component';
 import { FilesManagerComponent } from './views/files/files-manager.component';
@@ -20,95 +20,88 @@ import { ConfiguracionComponent } from './views/configuracion/configuracion.comp
 import { NotificationComponent } from './views/notifications/notification.component';
 import { BooksGridComponent } from './views/lectura-biblia/books-grid.component';
 
-
 export const routes: Routes = [
-    { path: "", component: HomeComponent },
-    { path: "home", component: HomeComponent },
-    { path: "modal", component: ModalComponent },
-    { path: "login", component: LoginComponent },
-    { path: "register", component: RegisterComponent },
-    {
-        path: "micuenta",
-        component: MicuentaComponent,
-        canActivate: [AuthGuard, RoleGuard],
-        data: { roles: ['user'] },
-    },
-    {
-        path: "tablero-anuncios",
-        component: TableroanunciosComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['user'] },
-    },
-    {
-        path: "tablero-anuncios/crear-anuncio",
-        component: CrearAnuncioComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['admin','user'] },
-    },
-    {
-        path: "tablero-anuncios/editar-anuncio/:id",
-        component: EditarAnuncioComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['admin','user'] },
-    },
-    {
-        path: "pdf-preview",
-        component: PdfPreviewComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['user'] },
-        
-    },
-    {
-        path: "grupos-servicio-campo",
-        component: gruposServiciodelcampoComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['user'] },
-        
-    },
-    {
-        path: "config",
-        component: ConfiguracionComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['admin','user'] },
-    },
-    {
-        path: "dashboard",
-        component: DashboardComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['admin'] },
-    },
-    {
-        path: "notifications",
-        component: NotificationComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['admin','user'] },
-    },
-    {
-        path: "mi-lectura-biblia",
-        component: BooksGridComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['user'] },
-    },
-    {
-        path: "files-manager",
-        component: FilesManagerComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['admin','user'] },
-    },
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'modal', component: ModalComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'micuenta',
+    component: MicuentaComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['user'] },
+  },
+  {
+    path: 'tablero-anuncios',
+    component: TableroanunciosComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['user'] },
+  },
+  {
+    path: 'tablero-anuncios/crear-anuncio',
+    component: CrearAnuncioComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['admin', 'user'] },
+  },
+  {
+    path: 'tablero-anuncios/editar-anuncio/:id',
+    component: EditarAnuncioComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['admin', 'user'] },
+  },
+  {
+    path: 'pdf-preview',
+    component: FilePreviewComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['user'] },
+  },
+  {
+    path: 'grupos-servicio-campo',
+    component: gruposServiciodelcampoComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['user'] },
+  },
+  {
+    path: 'config',
+    component: ConfiguracionComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['admin', 'user'] },
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['admin'] },
+  },
+  {
+    path: 'notifications',
+    component: NotificationComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['admin', 'user'] },
+  },
+  {
+    path: 'mi-lectura-biblia',
+    component: BooksGridComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['user'] },
+  },
+  {
+    path: 'files-manager',
+    component: FilesManagerComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['admin', 'user'] },
+  },
 
+  // Endpoints de prueba
+  {
+    path: 'test/admin',
+    component: AdminComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['admin'] },
+  },
 
-    // Endpoints de prueba
-    {
-        path: 'test/admin',
-        component: AdminComponent,
-        canActivate: [AuthGuard,RoleGuard],
-        data: { roles: ['admin'] },
-    },
-
-
-
-
-    // deben estar al final
-    { path: "unauthorized", component: UnauthorizedComponent },
-    { path: '**', component: NotFoundComponent },
+  // deben estar al final
+  { path: 'unauthorized', component: UnauthorizedComponent },
+  { path: '**', component: NotFoundComponent },
 ];
---
+++ src/app/views/pdf-preview/file-preview.component.ts
@@ -0,0 +1,111 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PdfViewerComponent } from '../../components/pdf-viewer/pdf-viewer.component';
+import { LoggerService } from '../../services/logger.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../../auth/auth.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-file-preview',
+  standalone: true,
+  imports: [CommonModule, PdfViewerComponent],
+  templateUrl: './file-preview.component.html',
+  styles: [],
+})
+export class FilePreviewComponent implements OnInit {
+  context: string = 'FilePreviewComponent';
+  fileSrc: string = ''; // URL del archivo
+  fileType: 'pdf' | 'image' | 'unknown' = 'unknown'; // Tipo de archivo
+  zoom!: number; // Nivel inicial de zoom
+  page: number = 1; // Página inicial
+  showAll: boolean = false; // Mostrar todas las páginas o una sola
+  imageUrl!: SafeUrl;
+
+  constructor(
+    private route: ActivatedRoute,
+    private logger: LoggerService,
+    private authService: AuthService,
+    private http: HttpClient,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    // Configuración inicial desde los parámetros de la URL
+    this.route.queryParams.subscribe((params) => {
+      this.fileSrc = params['filesrc'] || ''; // Obtener el parámetro "fileSrc"
+      this.page = Number(params['page']) || 1; // Asegurar que "page" sea un número
+      this.showAll = Number(params['showAll']) === 1; // Convertir number a booleano
+      this.logger.log(this.context, 'showAll =', this.showAll);
+
+      console.log('fileSrc', this.fileSrc);
+
+      // Determinar el tipo de archivo basándose en la extensión
+      const fileExtension = this.obtenerExtensionArchivo(this.fileSrc);
+      if (fileExtension === 'pdf') {
+        this.fileType = 'pdf';
+      } else if (
+        ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg'].includes(fileExtension)
+      ) {
+        this.fileType = 'image';
+        this.loadImage();
+      } else {
+        this.fileType = 'unknown';
+      }
+    });
+
+    // Configurar zoom según el ancho de la pantalla
+    this.adjustZoomToScreenWidth();
+  }
+
+  // Función para obtener la extensión del archivo
+  private obtenerExtensionArchivo(url: string): string {
+    const urlSinParametros = url.split(/[#?]/)[0];
+    const extension = urlSinParametros.split('.').pop()?.trim().toLowerCase();
+    return extension || '';
+  }
+
+  // Métodos públicos para ajustar el zoom
+  increaseZoom(): void {
+    if (this.zoom < 3) {
+      this.zoom += 0.1;
+    }
+  }
+
+  decreaseZoom(): void {
+    if (this.zoom > 0.2) {
+      this.zoom -= 0.1;
+    }
+  }
+
+  // Método para ajustar el zoom basado en el ancho de pantalla
+  private adjustZoomToScreenWidth(): void {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth < 576) {
+      this.zoom = 0.5;
+    } else if (screenWidth < 768) {
+      this.zoom = 0.75;
+    } else {
+      this.zoom = 1;
+    }
+    console.log(this.zoom);
+  }
+
+  loadImage(): void {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.authService.getToken()}`,
+    });
+
+    this.http.get(this.fileSrc, { headers, responseType: 'blob' }).subscribe({
+      next: (blob: Blob) => {
+        const objectURL = URL.createObjectURL(blob);
+        this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+      },
+      error: (error) => {
+        console.error('Error al cargar la imagen', error);
+      },
+    });
+  }
+}
